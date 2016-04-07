a1a65bba1893b9f3e1a2b6712276efc921068dc2
= <=>
2016-04-07 20:00:36 +0300

    first commit

---
+++ MindBox/TriangleMath.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MindBox
+{
+    public static class TriangleMath
+    {
+        public static double GetRightTriangleSquare(double side1, double side2, double side3)
+        {
+            return GetTriangleSquare(side1, side2, side3);
+        }
+        public static double GetRightTriangleSquare(double cathetus1, double cathetus2)
+        {
+            if (cathetus1 <= 0 || cathetus2 <= 0)
+            {
+                throw new ArgumentException("Sides length must be positive.");
+            }
+            return cathetus1*cathetus2/2;
+        }
+        public static double GetTriangleSquare(double side1, double side2, double side3)
+        {
+            if (side1 <= 0 || side2 <= 0 || side3 <= 0)
+            {
+                throw new ArgumentException("Sides length must be positive.");
+            }
+            if (side1 >= side2 + side3 || side2 >= side1 + side3 || side3 >= side1 + side2)
+            {
+                throw new ArgumentException("Every side must be less than sum of others.");
+            }
+            var p = (side1 + side2 + side3)/2;
+            return Math.Sqrt(p * (p - side1) * (p - side2) * (p - side3)); 
+        }
+    }
+}
---
+++ UnitTests/TriangleMathTests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using MindBox;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    public class TriangleMathTests
+    {
+        [TestCase(0, 5, 5)]
+        [TestCase(5, 0, 5)]
+        [TestCase(5, 5, 0)]
+        [TestCase(-5, 5, 5)]
+        [TestCase(5, -5, 5)]
+        [TestCase(5, 5, -5)]
+        public void GetTriangleSquare_ZeroOrNegativeSide_ExceptionThrown(double side1, double side2, double side3)
+        {
+            var ex = Assert.Throws<ArgumentException>(() => TriangleMath.GetTriangleSquare(side1, side2, side3));
+            Assert.AreEqual(ex.Message, "Sides length must be positive.");
+        }
+
+        [TestCase(10, 5, 5)]
+        [TestCase(5, 10, 5)]
+        [TestCase(5, 5, 10)]
+        [TestCase(20, 5, 5)]
+        [TestCase(5, 20, 5)]
+        [TestCase(5, 5, 20)]
+        public void GetTriangleSquare_SideMoreOrEqualThanSumOfOthers_ExceptionThrown(double side1, double side2, double side3)
+        {
+            var ex = Assert.Throws<ArgumentException>(() => TriangleMath.GetTriangleSquare(side1, side2, side3));
+            Assert.AreEqual(ex.Message, "Every side must be less than sum of others.");
+        }
+
+        [TestCase(5, 5, 5, 10.825)]
+        public void GetTriangleSquare_CorrectData_CorrectResult(double side1, double side2, double side3, double expectingResult)
+        {
+            //Arrange
+            double epsilon = 1E-3;
+            //Act
+            var result = TriangleMath.GetTriangleSquare(side1, side2, side3);
+            //Assert
+            Assert.Less(Math.Abs(result - expectingResult), epsilon);
+        }
+
+        [TestCase(0, 5)]
+        [TestCase(5, 0)]
+        [TestCase(-5, 5)]
+        [TestCase(5, -5)]
+        public void GetRightTriangleSquareCathetusArgs_ZeroOrNegativeSide_ExceptionThrown(double cathetus1, double cathetus2)
+        {
+            var ex = Assert.Throws<ArgumentException>(() => TriangleMath.GetRightTriangleSquare(cathetus1, cathetus2));
+            Assert.Throws(typeof(ArgumentException), () => TriangleMath.GetRightTriangleSquare(cathetus1, cathetus2), "Sides length must be positive.");
+        }
+
+        [TestCase(5, 5, 12.5)]
+        public void GetRightTriangleSquareCathetusArgs_CorrectData_CorrectResult(double cathetus1, double cathetus2, double expectingResult)
+        {
+            //Arrange
+            double epsilon = 1E-3;
+            //Act
+            var result = TriangleMath.GetRightTriangleSquare(cathetus1, cathetus2);
+            //Assert
+            Assert.Less(Math.Abs(result - expectingResult), epsilon);
+        }
+    }
+}
